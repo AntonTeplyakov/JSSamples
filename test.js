50a1c7930f621768bd30a7adc589f52f74552a02
AntonTeplyakov <jsxscript@gmail.com>
2021-07-02 10:35:16 +1000

    sample callback func

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("Test");
+const ul = document.querySelector('.people');
+
+arr = ['Jack', 'Anton', 'Jane', 'Lee'];
+
+arr.forEach((people, ind, i) => {
+    console.log(people, ind, i);
+});
+
