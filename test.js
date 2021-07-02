043a1942eefb078c58ef3eeffa35076d346848f3
AntonTeplyakov <jsxscript@gmail.com>
2021-07-02 10:46:06 +1000

    Sample ul list with dynamic generation with JavaScript.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const ul = document.querySelector('.people');
+const ul = document.querySelector('ul');
 
 arr = ['Jack', 'Anton', 'Jane', 'Lee'];
 
-arr.forEach((people, ind, i) => {
-    console.log(people, ind, i);
+arr.forEach((people, ind) => {
+    ul.innerHTML += `<li>The best score: ${ind+1}. ${people}</li>`
 });
 
