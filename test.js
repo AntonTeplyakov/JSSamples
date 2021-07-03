70bb147d08ed35a517a130c984f5bef1e482007f
Anton Teplyakov <jshint@yandex.ru>
2021-07-03 11:00:39 +1000

    dynamic add class to <p>

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 const pl = document.querySelectorAll('p');
+const h1 = document.querySelector('.title');
+
+h1.classList.toggle('title');
+h1.classList.toggle('title');
 
 pl.forEach(p => {
     console.log(p.textContent);
     if (p.textContent.includes('error')){
     p.classList.add('error')}
 });
+
