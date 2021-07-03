f7cff18dff04d8045d2f17133f961c3cb1ca3bb9
Anton Teplyakov <jsxscript@gmail.com>
2021-07-03 11:22:04 +1000

    empty

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const h1 = document.querySelector('.title');
 h1.classList.toggle('title');
 h1.classList.toggle('title');
 
+
 pl.forEach(p => {
     console.log(p.textContent);
     if (p.textContent.includes('error')){
