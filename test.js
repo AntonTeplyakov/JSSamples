1f97afcccac8333a88a9a0749fe595815680a4cf
AntonTeplyakov <jsxscript@gmail.com>
2021-07-22 13:04:44 +1000

    array methods, filter

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-const li = document.querySelectorAll('li');
+// const li = document.querySelectorAll('li');
 
-li.forEach(item=>{
-    item.addEventListener('click',e =>{
-        // e.target.style.textDecoration = 'line-through';
-        e.target.remove();
-    });
+// li.forEach(item=>{
+//     item.addEventListener('click',e =>{
+//         // e.target.style.textDecoration = 'line-through';
+//         e.target.remove();
+//     });
+// });
+
+const arr = [10, 15, 40, 5, 60, 35, 25, 45, 5, 10];
+
+const result = arr.filter((arr) => {
+    return arr < 20;
 });
 
+console.log(result);
