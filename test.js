e0c99066ef3d18254d733a0d8000c769e57ce1fa
AntonTeplyakov <jsxscript@gmail.com>
2021-07-29 14:17:41 +1000

    sample date & time methods

--- test.js
+++ test.js
@@ -7,10 +7,26 @@
 //     });
 // });
 
-const arr = [10, 15, 40, 5, 60, 35, 25, 45, 5, 10];
+// const arr = [10, 15, 40, 5, 60, 35, 25, 45, 5, 10];
 
-const result = arr.map((arr) => {
-    return arr = arr / 5;
-});
+// const result = arr.map((arr) => {
+//     return arr = arr / 5;
+// });
 
-console.log(result);
+// console.log(result);
+
+const now = new Date();
+
+console.log(now);
+console.log("Year: ", now.getFullYear());
+console.log("Month: ", now.getMonth());
+console.log("Day: ", now.getDay());
+console.log("Hours: ", now.getHours());
+console.log("Minutes: ", now.getMinutes());
+console.log("Seconds: ", now.getSeconds());
+
+console.log("Time: ", now.getTime());
+
+console.log("ShortDate", now.toDateString());
+console.log("ShortTime", now.toTimeString());
+console.log("System", now.toLocaleString());
