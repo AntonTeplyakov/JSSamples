17aef139b1ae6d885a90293bf8990570c860d2a2
AntonTeplyakov <jsxscript@gmail.com>
2021-08-13 18:10:15 +1000

    about classes in JavaScript

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
-const patr = document.querySelector("p");
+class User {
+    constructor(username,email){
+        this.username = username;
+        this.email = email;
+        this.score = 0;
+    }
 
-console.log(patr.getAttribute('class'));
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;   
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);   
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
+    }
 
-patr.setAttribute('class', 'success');
-patr.innerText = "Succesful login!";
+}
+
+const userOne = new User('dfgdfdg','ertretrrt@dfgdfgd.com');
+const userTwo = new User('fdhdfh','dfgdfgd@dfgdfgd.com');
+
+console.log(userOne.login().logout());
+console.log(userTwo.login().incScore().incScore());
