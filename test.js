6ec0ffa12a32e3f54b9b9fc6bcd024a543ee2d21
AntonTeplyakov <jsxscript@gmail.com>
2021-08-13 13:42:42 +1000

    sample save,read and delete items from localStorage

--- test.js
+++ test.js
@@ -1,42 +1,11 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+localStorage.setItem("Anton", 34);
 
-  request.addEventListener("readystatechange", () => {
-    console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-      console.log(request, request.responseText);
-    } else if (request.readyState === 4);
-    {
-      callback("could not fetch data", undefined);
-    }
-  });
+let age = localStorage.Anton;
 
-  request.open("GET", resource);
-  request.send();
-};
+console.log(age);
 
-const getSomething = () => {
-    return new Promise((resolve,reject) =>{
-        //fetch something
-        resolve('some data');
-       // reject('some error');
-    });
-};
+localStorage.removeItem("Anton");
 
-getSomething().then((data) =>{
-  console.log(data);
-},(err) =>{
-  console.log(err);
-});
+let my_age = localStorage.getItem("Anton");
 
-
-
-// let resource = "https://jsonplaceholder.typicode.com/posts";
-
-// getTodos(resource, (err, data) => {
-//   console.log(data);
-// });
-
-
+console.log(my_age);
