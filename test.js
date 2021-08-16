724acff74fc7a2ce646623b96d0ec185947a8d1e
AntonTeplyakov <jsxscript@gmail.com>
2021-08-16 13:28:03 +1000

    class realization by prototypes, also polymorphism & overloading

--- test.js
+++ test.js
@@ -1,47 +1,67 @@
-class User {
-    constructor(username, email) {
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-
-    login() {
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout() {
-        console.log(`${this.username} just logged out`);
-        return this;
-    }
-    incScore() {
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
-    }
-
+function User(username, email){
+    this.username = username;
+    this.email = email;
 }
 
-class Admin extends User {
-    constructor(username, email, title) {
-        super(username, email);
-        this.title = title;
-    }
-    deleteUser(user) {
-        users = users.filter((u) => {
-            return u.username !== user.username
-        })
-    }
+User.prototype.login = function(){
+    console.log(`${this.username} has logged in`)
+    return this;
+};
+
+User.prototype.logout = function(){
+    console.log(`${this.username} has logged out`)
+    return this;
+};
+
+function Admin(username,email,title){
+    User.call(this,username,email);
+    this.title = title;
 }
 
-const userOne = new User('Jane', 'mirror@jeneva.com');
-const userTwo = new User('Richard', 'gray@tea.com');
-const userThree = new Admin('Anton', 'jsxlabor@SpeechGrammarList.com', 'blank');
+Admin.prototype = Object.create(User.prototype);
+// Admin.prototype = (User.prototype);
 
 
-let users = [userOne, userTwo, userThree];
+Admin.prototype.login = function(){
+    console.log(`${this.username} has own logIn method!`);
+}
 
-userThree.deleteUser(userTwo);
 
-console.log(userOne, userTwo, userThree);
-userThree.incScore();
-console.log(users);
+
+
+const userOne = new User('dfgdfdg','ertretrrt@dfgdfgd.com');
+const userTwo = new User('fdhdfh','dfgdfgd@dfgdfgd.com');
+const userThree = new Admin('Anton','tr@fg.com','Working!');
+
+console.log(userOne,userTwo,userThree);
+userOne.login();
+userThree.login();
+userOne.login();
+// class User {
+//     constructor(username,email){
+//         this.username = username;
+//         this.email = email;
+//         this.score = 0;
+//     }
+
+//     login(){
+//         console.log(`${this.username} just logged in`);
+//         return this;   
+//     }
+//     logout(){
+//         console.log(`${this.username} just logged out`);   
+//         return this;
+//     }
+//     incScore(){
+//         this.score += 1;
+//         console.log(`${this.username} has a score of ${this.score}`);
+//         return this;
+//     }
+
+// }
+
+// const userOne = new User('dfgdfdg','ertretrrt@dfgdfgd.com');
+// const userTwo = new User('fdhdfh','dfgdfgd@dfgdfgd.com');
+
+// console.log(userOne.login().logout());
+// console.log(userTwo.login().incScore().incScore());
