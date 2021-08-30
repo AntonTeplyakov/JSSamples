65a19c1f6c839c4aa9d147fc8d5f853efa2053ca
AntonTeplyakov <jsxscript@gmail.com>
2021-08-31 09:22:38 +1000

    sample working with Proxy

--- test.js
+++ test.js
@@ -1,42 +1,42 @@
-function User(username, email){
-    this.username = username;
-    this.email = email;
-}
+// function User(username, email){
+//     this.username = username;
+//     this.email = email;
+// }
 
-User.prototype.login = function(){
-    console.log(`${this.username} has logged in`)
-    return this;
-};
+// User.prototype.login = function(){
+//     console.log(`${this.username} has logged in`)
+//     return this;
+// };
 
-User.prototype.logout = function(){
-    console.log(`${this.username} has logged out`)
-    return this;
-};
+// User.prototype.logout = function(){
+//     console.log(`${this.username} has logged out`)
+//     return this;
+// };
 
-function Admin(username,email,title){
-    User.call(this,username,email);
-    this.title = title;
-}
+// function Admin(username,email,title){
+//     User.call(this,username,email);
+//     this.title = title;
+// }
 
-Admin.prototype = Object.create(User.prototype);
-// Admin.prototype = (User.prototype);
-
-
-Admin.prototype.login = function(){
-    console.log(`${this.username} has own logIn method!`);
-}
+// Admin.prototype = Object.create(User.prototype);
+// // Admin.prototype = (User.prototype);
 
 
+// Admin.prototype.login = function(){
+//     console.log(`${this.username} has own logIn method!`);
+// }
 
 
-const userOne = new User('dfgdfdg','ertretrrt@dfgdfgd.com');
-const userTwo = new User('fdhdfh','dfgdfgd@dfgdfgd.com');
-const userThree = new Admin('Anton','tr@fg.com','Working!');
 
-console.log(userOne,userTwo,userThree);
-userOne.login();
-userThree.login();
-userOne.login();
+
+// const userOne = new User('dfgdfdg','ertretrrt@dfgdfgd.com');
+// const userTwo = new User('fdhdfh','dfgdfgd@dfgdfgd.com');
+// const userThree = new Admin('Anton','tr@fg.com','Working!');
+
+// console.log(userOne,userTwo,userThree);
+// userOne.login();
+// userThree.login();
+// userOne.login();
 // class User {
 //     constructor(username,email){
 //         this.username = username;
@@ -66,8 +66,23 @@ userOne.login();
 // console.log(userOne.login().logout());
 // console.log(userTwo.login().incScore().incScore());
 
-const m = ([...spr])=>{
-    console.log(spr);
+// const m = ([...spr])=>{
+//     console.log(spr);
+// };
+
+// m(new Set(['dfgdfg', 'sdgdsg','gdsfgdg']));
+
+var handler = {
+    get: function(target, name){
+        return name in target?
+            target[name] :
+            'hello';
+    }
 };
 
-m(new Set(['dfgdfg', 'sdgdsg','gdsfgdg']));
+var p = new Proxy({}, handler);
+p.a = 1;
+p.b = undefined;
+
+console.log(p.a, p.b); // 1, undefined
+console.log('c' in p, p.c); // false, 37
